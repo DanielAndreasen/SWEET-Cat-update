a09c698d368a1f9c2fc951297dadb741b6680189
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-01-06 14:39:46 +0000

    Add outliers to the plot as from the discussion section

--- figures/comparison.py
+++ figures/comparison.py
@@ -23,6 +23,8 @@ if __name__ == '__main__':
              'erlogglc', 'vt', 'ervt', 'metal', 'ermetal', 'mass', 'ermass',
              'author', 'source', 'update', 'comment', 'z1', 'z2']
 
+    outliers = ['HAT-P-46', 'HD 120084', 'HD 233604', 'HD 5583', 'HD 81688',
+                'HIP 107773', 'WASP-97', 'ome Ser', 'omi UMa']
     df = pd.read_csv('SC.csv')
     # df = df[df.convergence]
     sc = pd.read_csv('WEBSITE_online.rdb', delimiter='\t', names=names, na_values=['NULL'])
@@ -53,6 +55,7 @@ if __name__ == '__main__':
     df2['vtdiff'] = df2['vt']-df2['vtnew']
 
     df = df2  # Easier than renaming all below to df2...
+    do = df.loc[df['star'].isin(outliers)]
     stat = df.describe()
 
     teffdiff = stat.loc['mean', 'teffdiff']
@@ -69,12 +72,14 @@ if __name__ == '__main__':
     gs1.update(right=0.48, left=0.05, bottom=0.55, hspace=0.10)
     ax1 = plt.subplot(gs1[0:2, 0])
     ax1.plot(df.teff, df.teffnew, 'o', alpha=0.7)
+    ax1.plot(do.teff, do.teffnew, 'or', alpha=0.7)
     ax1.errorbar([4200*1.05], [7100*0.95], xerr=tx, yerr=ty, fmt='-k')
     ax1.plot([4200, 7100], [4200, 7100], '--k', lw=3)
     ax1.set_xticklabels([])
     ax1.set_title('Teff')
     ax2 = plt.subplot(gs1[-1, 0])
     ax2.plot(df.teff, df.teffdiff, 'o', alpha=0.7)
+    ax2.plot(do.teff, do.teffdiff, 'or', alpha=0.7)
     ax2.hlines([teffdiff], 4200, 7100)
     ax2.set_yticks([-400, -200, 0, 200, 400])
     ax2.set_ylim(-500, 500)
@@ -84,12 +89,14 @@ if __name__ == '__main__':
     gs2.update(right=0.48, left=0.05, top=0.45, hspace=0.05)
     ax3 = plt.subplot(gs2[0:2, 0])
     ax3.plot(df.logg, df.loggnew, 'o', alpha=0.7)
+    ax3.plot(do.logg, do.loggnew, 'or', alpha=0.7)
     ax3.errorbar([1.8*1.05], [5.1*0.95], xerr=gx, yerr=gy, fmt='-k')
     ax3.plot([1.8, 5.1], [1.8, 5.1], '--k', lw=3)
     ax3.set_xticklabels([])
     ax3.set_title('log g')
     ax4 = plt.subplot(gs2[-1, 0])
     ax4.plot(df.logg, df.loggdiff, 'o', alpha=0.7)
+    ax4.plot(do.logg, do.loggdiff, 'or', alpha=0.7)
     ax4.hlines([loggdiff], 1.8, 5.1)
     ax4.set_yticks([-0.6, -0.3, 0.0, 0.3, 0.6])
     ax4.set_ylim(-0.9, 0.9)
@@ -99,12 +106,14 @@ if __name__ == '__main__':
     gs3.update(left=0.55, right=0.98, bottom=0.55, hspace=0.05)
     ax5 = plt.subplot(gs3[0:2, 0])
     ax5.plot(df.metal, df.fehnew, 'o', alpha=0.7)
+    ax5.plot(do.metal, do.fehnew, 'or', alpha=0.7)
     ax5.errorbar([-0.8*0.95], [0.55*0.65], xerr=fx, yerr=fy, fmt='-k')
     ax5.plot([-0.8, 0.55], [-0.8, 0.55], '--k', lw=3)
     ax5.set_xticklabels([])
     ax5.set_title('[Fe/H]')
     ax6 = plt.subplot(gs3[-1, 0])
     ax6.plot(df.metal, df.fehdiff, 'o', alpha=0.7)
+    ax6.plot(do.metal, do.fehdiff, 'or', alpha=0.7)
     ax6.hlines([fehdiff], -0.8, 0.55)
     ax4.set_yticks([-0.6, -0.3, 0.0, 0.3, 0.6])
     ax6.set_ylim(-0.5, 0.5)
@@ -114,12 +123,14 @@ if __name__ == '__main__':
     gs4.update(left=0.55, right=0.98, top=0.45, hspace=0.05)
     ax7 = plt.subplot(gs4[0:2, 0])
     ax7.plot(df.vt, df.vtnew, 'o', alpha=0.7)
+    ax7.plot(do.vt, do.vtnew, 'or', alpha=0.7)
     ax7.errorbar([-0.1*0.95], [2.2*0.95], xerr=vx, yerr=vy, fmt='-k')
     ax7.plot([-0.1, 2.2], [-0.1, 2.2], '--k', lw=3)
     ax7.set_xticklabels([])
     ax7.set_title('Microturbulence')
     ax8 = plt.subplot(gs4[-1, 0])
     ax8.plot(df.vt, df.vtdiff, 'o', alpha=0.7)
+    ax8.plot(do.vt, do.vtdiff, 'or', alpha=0.7)
     ax8.hlines([vtdiff], -0.1, 2.2)
     ax8.set_yticks([-0.8, -0.4, 0.0, 0.4, 0.8])
     ax8.set_ylim(-1.0, 1.0)
